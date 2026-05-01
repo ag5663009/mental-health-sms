cf48f9f50830462d009a5b2a5695bef691232ded
ag5663009 <ag5663009@gmail.com>
2026-04-30 21:24:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+const { MessagingResponse } = require("twilio").twiml;
+
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+  twiml.message("Your SMS assistant is online.");
+
+  res.type("text/xml");
+  res.send(twiml.toString());
+});
+
